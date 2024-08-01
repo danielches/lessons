1b8ba11aa20a8da7fca3dce77c681a87170b9157
danielches <65138234+danielches@users.noreply.github.com>
2024-08-01 17:24:09 +0300

    module2_3.py

--- module2_3.py
+++ module2_3.py
@@ -1,7 +1,7 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
 while True:
-    if my_list[i] < 0 or i >= len(my_list):
+    if i >= len(my_list) or my_list[i] < 0:
         break
     if my_list[i] == 0:
         i += 1
