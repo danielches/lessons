76d5cdfb2d01b06454402ef1404d6ca5bb8e37b4
danielches <65138234+danielches@users.noreply.github.com>
2024-08-01 18:41:07 +0300

    module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,12 +1,14 @@
-n = int(input("Введите число от 3 до 20\n"))
-while (n < 3 or n > 20):
-    n = int(input("Введите число от 3 до 20\n"))
-n = 3
-while n < 21:
-    print(f"{n} = ", end="")
+def resh(n):
+    result = ""
     for first in range(1, n):
-        for second in range(first+1, n):
+        for second in range(first + 1, n):
             if n % (first + second) == 0:
-                print(f"{first}{second}", end="")
-    print()
-    n += 1
+                result += f"{first}{second}"
+    return result
+
+
+n = int(input("Введите число от 3 до 20\n"))
+while n < 3 or n > 20:
+    n = int(input("Введите число от 3 до 20\n"))
+
+print(resh(n))
