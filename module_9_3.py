c9973489821b84dcee695cc593dfcfdeb8184b72
danielches <65138234+danielches@users.noreply.github.com>
2024-08-18 16:37:03 +0300

    module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -1,7 +1,7 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = (abs(len(fir) - len(sec)) for fir,sec in zip(first,second) if len(fir)-len(sec) != 0)
+first_result = (abs(len(fir) - len(sec)) for fir,sec in zip(first,second) if len(fir) - len(sec) != 0)
 second_result = (len(first[i]) == len(second[i]) for i in range(min(len(first), len(second))))
 
 print(list(first_result))
