cf135194e4ea6633676809516adccc2f320cf24b
danielches <65138234+danielches@users.noreply.github.com>
2024-08-18 22:02:43 +0300

    Add files via upload

---
+++ module_7_1.py
@@ -0,0 +1,40 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f"{self.name}, {self.weight}, {self.category}"
+
+
+class Shop:
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        res = file.read()
+        file.close()
+        return res
+
+    def add(self, *products):
+        file = open(self.__file_name, 'a+')
+        for product in products:
+            products_in_file = [text.split(', ')[0] for text in self.get_products().split('\n')]
+            if product.name not in products_in_file:
+                file.write(f"{product}\n")
+            else:
+                print(f"Продукт {product} уже есть в магазине")
+        file.close()
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)  # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
