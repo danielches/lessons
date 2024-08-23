8824df638890d113804a1cd1313ff1937fedc0d5
danielches <65138234+danielches@users.noreply.github.com>
2024-08-24 02:25:01 +0300

    module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -1,7 +1,6 @@
 import os
 import time
-#Научиться применять методы os.walk, os.path.join, os.path.getmtime, os.path.dirname, os.path.getsize и использование
-# модуля time для корректного отображения времени.
+
 directory = r'C:\Users\danil\OneDrive\Рабочий стол\UrbanUniversityHomework\module7'
 
 for root, dirs, files in os.walk(directory):
@@ -13,27 +12,3 @@ for root, dirs, files in os.walk(directory):
         parent_dir = os.path.dirname(file)
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, '
               f'Родительская директория: {parent_dir}')
-'''Используйте os.walk для обхода каталога, путь к которому указывает переменная directory
-Примените os.path.join для формирования полного пути к файлам.
-Используйте os.path.getmtime и модуль time для получения и отображения времени последнего изменения файла.
-Используйте os.path.getsize для получения размера файла.
-Используйте os.path.dirname для получения родительской директории файла.
-
-Комментарии к заданию:
-
-Ключевая идея – использование вложенного for
-
-for root, dirs, files in os.walk(directory):
-  for file in files:
-    filepath = ?
-    filetime = ?
-    formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-    filesize = ?
-    parent_dir = ?
-    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
-
-
-
-Так как в разных операционных системах разная схема расположения папок, тестировать проще всего в папке проекта (directory = “.”)
-Пример возможного вывода:
-Обнаружен файл: main.py, Путь: ./main.py, Размер: 111 байт, Время изменения: 11.11.1111 11:11, Родительская директория.'''
