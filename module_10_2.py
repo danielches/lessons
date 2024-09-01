5c91bec3dc038d1c62f10b7eb438fd5f5465f358
danielches <65138234+danielches@users.noreply.github.com>
2024-09-01 11:54:17 +0300

    Add files via upload

---
+++ module_10_2.py
@@ -0,0 +1,38 @@
+from threading import Thread
+from time import sleep
+
+
+class Knight(Thread):
+    cnt_enemies = 100
+
+    def __init__(self, name, power):
+        super().__init__()
+        self.name = name
+        self.power = power
+
+    def run(self):
+        print(f"{self.name}, на нас напали!")
+        cnt_def_enemies = 0
+        cnt_days = 1
+        while cnt_def_enemies != self.cnt_enemies:
+            if cnt_def_enemies + self.power > self.cnt_enemies:
+                cnt_def_enemies += self.cnt_enemies - self.power
+            else:
+                cnt_def_enemies += self.power
+            sleep(1)
+            print(
+                f"{self.name} сражается {cnt_days} день(дня)..., осталось {self.cnt_enemies - cnt_def_enemies} воинов.\n",
+                end='')
+            cnt_days += 1
+
+
+# Создание класса
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+# Запуск потоков и остановка текущего
+first_knight.start()
+second_knight.start()
+
+first_knight.join()
+second_knight.join()
+print("Все битвы закончились!")
