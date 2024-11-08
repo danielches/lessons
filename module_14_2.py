499975f120adff6ebf9767cb03c7ef2794fd8755
danielches <65138234+danielches@users.noreply.github.com>
2024-11-08 22:48:45 +0300

    Add files via upload

---
+++ module_14_2.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL)
+''')
+'''cursor.execute("DELETE FROM Users WHERE id = ?", (6,))'''
+cursor.execute("SELECT COUNT(*) FROM Users")
+cnt_users = cursor.fetchone()[0]
+cursor.execute("SELECT SUM(balance) FROM Users")
+sum_balance_all_users = cursor.fetchone()[0]
+cursor.execute("SELECT AVG(balance) FROM Users")
+avg_balance_all_users = cursor.fetchone()[0]
+print(cnt_users)
+print(sum_balance_all_users)
+print(avg_balance_all_users, sum_balance_all_users/cnt_users)
+
+
+connection.commit()
+connection.close()
