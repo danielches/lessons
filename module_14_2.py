ad6461c8f88dff4e51333c76e3903fabb2778de7
danielches <65138234+danielches@users.noreply.github.com>
2024-11-08 22:50:05 +0300

    module_14_2.py

--- module_14_2.py
+++ module_14_2.py
@@ -11,7 +11,7 @@ email TEXT NOT NULL,
 age INTEGER,
 balance INTEGER NOT NULL)
 ''')
-'''cursor.execute("DELETE FROM Users WHERE id = ?", (6,))'''
+cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
 cursor.execute("SELECT COUNT(*) FROM Users")
 cnt_users = cursor.fetchone()[0]
 cursor.execute("SELECT SUM(balance) FROM Users")
@@ -21,7 +21,6 @@ avg_balance_all_users = cursor.fetchone()[0]
 print(cnt_users)
 print(sum_balance_all_users)
 print(avg_balance_all_users, sum_balance_all_users/cnt_users)
-
 
 connection.commit()
 connection.close()
