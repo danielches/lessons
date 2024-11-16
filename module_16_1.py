0e7501e4c73a5461f9783d325207ffc2cc836d51
danielches <65138234+danielches@users.noreply.github.com>
2024-11-17 01:44:14 +0300

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+import logging
+
+logging.basicConfig(level=logging.INFO)
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "Главная страница"}
+
+
+@app.get("/user/admin")
+async def welcome() -> dict:
+    return {"message": "Вы вошли как администратор"}
+
+
+@app.get("/user/{user_id}")
+async def welcome(user_id: str) -> dict:
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+
+@app.get("/user/")
+async def welcome(username: str, age: int) -> dict:
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+
+@app.get("/id/")
+async def welcome(username: str, age: int) -> dict:
+    return {"message": f"Информация о пользователе."}
