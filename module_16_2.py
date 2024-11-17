6515dc1992b22e7c24aa76e117052e7c8808386d
danielches <65138234+danielches@users.noreply.github.com>
2024-11-17 03:01:02 +0300

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+import logging
+logging.basicConfig(level=logging.INFO)
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "Главная страница"}
+
+
+@app.get("/user/admin")
+async def welcome() -> dict:
+    return {"message": "Вы вошли как администратор"}
+
+
+@app.get("/user/{user_id}")
+async def welcome(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")]) -> dict:
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+
+@app.get("/user/")
+async def welcome(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example= "UrbanUser")],
+                  age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]) -> dict:
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+
