fb37d07e63c9e44936fcdb5fc847b0e15c5f64c2
danielches <65138234+danielches@users.noreply.github.com>
2024-11-17 23:39:01 +0300

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Path
 from typing import Annotated
 import logging
+
 logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
@@ -11,17 +12,17 @@ async def welcome() -> dict:
 
 
 @app.get("/user/admin")
-async def welcome() -> dict:
+async def login_admin() -> dict:
     return {"message": "Вы вошли как администратор"}
 
 
 @app.get("/user/{user_id}")
-async def welcome(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")]) -> dict:
+async def login_user(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")]) -> dict:
     return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 
-@app.get("/user/")
-async def welcome(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example= "UrbanUser")],
-                  age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]) -> dict:
+@app.get("/user/{username}/{age}")
+async def info_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username",
+                                                  example="UrbanUser")],
+                    age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]) -> dict:
     return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
-
